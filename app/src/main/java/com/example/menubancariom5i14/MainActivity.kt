1da7fcc50b804a74178b622993cdea31a8ee2d29
Enzo_Gatica <enzogatica96@gmail.com>
2023-07-17 22:33:43 -0400

    Finalizado, opciones funcionales

--- app/src/main/java/com/example/menubancariom5i14/MainActivity.kt
+++ app/src/main/java/com/example/menubancariom5i14/MainActivity.kt
@@ -2,10 +2,52 @@ package com.example.menubancariom5i14
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import com.example.menubancariom5i14.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    private var saldo = 0;
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.button.setOnClickListener{v ->
+            when (binding.radioGroup.checkedRadioButtonId){
+                R.id.radioVerSaldo ->
+                    binding.editTextDos.setText("Saldo: " + saldo.toString())
+                R.id.radioIngresar ->
+                    ingresarSaldo()
+                R.id.radioRetirar ->
+                    retirarSaldo()
+                R.id.radioSalir ->
+                    salir()
+            }
+        }
+
+    }
+
+    fun ingresarSaldo(){
+        saldo += binding.editTextUno.text.toString().toInt()
+        binding.editTextDos.setText("Saldo: " + saldo.toString())
+        Toast.makeText(applicationContext,"Su saldo ha sido ingresado correctamente", Toast.LENGTH_LONG).show()
+    }
+
+    fun retirarSaldo(){
+        val monto = binding.editTextUno.text.toString().toInt()
+        if(monto <= saldo){
+            saldo -= monto
+            binding.editTextUno.text.clear()
+            Toast.makeText(applicationContext,"Su saldo ha sido retirado correctamente", Toast.LENGTH_LONG).show()
+        }else{
+            Toast.makeText(applicationContext,"no tiene saldo suficiente", Toast.LENGTH_LONG).show()
+        }
+    }
+
+
+    private fun salir() {
+        System.exit(0)
     }
 }
